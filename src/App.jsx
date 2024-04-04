1ccd757bd29ff77f0c1fcb2afe941a2e15b0f231
Kaio <kaio.meireles@outlook.com>
2024-04-03 22:36:39 -0300

    add add

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { createBrowserRouter,RouterProvider} from 'react-router-dom'
 import Nav from './components/Nav';
 import Footer from './components/Footer';
 import Home from './components/Home';
